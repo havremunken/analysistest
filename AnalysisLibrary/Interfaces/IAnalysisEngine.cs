f382a7407a041f3ddedc249b8743c4be58904dd1
Rune Jacobsen <havremunken@gmail.com>
2010-05-20 20:57:46 +0200

    Added a wee bit of structure to the library project

---
+++ AnalysisLibrary/Interfaces/IAnalysisEngine.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AnalysisLibrary.Interfaces
+{
+    public interface IAnalysisEngine
+    {
+        string Name { get; }
+
+        event Action ResultsUpdated;
+
+        event Action ProcessFinished;
+
+        void AnalyzeOutcomes(IEnumerable<AnalysisBundle> bundles);
+    }
+}
